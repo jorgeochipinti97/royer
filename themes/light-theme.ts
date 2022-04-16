f3e421a21b17c279554a952a1898ed3aba544193
jorge ochipinti <jorgeochipinti97@gmail.com>
2022-04-16 18:23:12 -0300

    commit

--- themes/light-theme.ts
+++ themes/light-theme.ts
@@ -64,10 +64,10 @@ export const lightTheme = createTheme({
           textTransform: 'none',
           boxShadow: 'none',
           borderRadius: 10,
-          ":hover": {
-            backgroundColor: 'rgba(0,0,0,0.05)',
-            transition: 'all 0.3s ease-in-out'
-          }
+          // ":hover": {
+          //   backgroundColor: 'rgba(0,0,0,0.65)',
+          //   transition: 'all 0.3s ease-in-out'
+          // }
         }
       }
     },
