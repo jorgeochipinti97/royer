99c3da683fc68f9fccc0f3baecadcd7987ba4a16
jorge ochipinti <jorgeochipinti97@gmail.com>
2022-04-18 08:48:48 -0300

    sort

--- components/products/ProductFilter.tsx
+++ components/products/ProductFilter.tsx
@@ -1,7 +1,7 @@
-import { useState, FC, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { FullScreenLoading } from '../../components/ui';
 
-import { capitalize, Box, Button, Divider } from '@mui/material';
+import { capitalize, Box, Button, Divider, InputLabel, Select, MenuItem, FormControl } from '@mui/material';
 import { useProducts } from '../../hooks';
 import { ProductList } from './ProductList';
 import { IProduct } from '../../interfaces';
@@ -14,12 +14,46 @@ export const ProductFilterPage = () => {
     const [_productsFiltered, setProductsFiltered] = useState<IProduct[]>(products)
     const [valueProduct, setValueProduct] = useState<string>('all')
     const [typeProduct, setTypeProduct] = useState<string>('')
-    const genders_ = ['all','kid', 'regionales', 'fashion']
+    const genders_ = ['all', 'kid', 'regionales', 'fashion']
     const todasCategorias = ['shirts', 't-shirt', 'football shirt', 'jacket', 'pants', 'hoodies', 'hats', 'mate', 'yerba', 'alfajores', 'wine', 'short', 'socks', 'wallet', 'purse', 'accessories', 'bag']
     const categoriasRopa = ['shirts', 't-shirt', 'football shirt', 'jacket', 'pants', 'hoodies', 'hats', 'short', 'socks', 'wallet', 'purse']
-    const fashion__ = ['wallet', 'purse', 'shirts', 'bag','hats']
+    const fashion__ = ['wallet', 'purse', 'shirts', 'bag', 'hats']
     const categoriasRegional = ['mate', 'yerba', 'alfajores', 'wine']
     const [categories, setCategories] = useState<string[]>(todasCategorias)
+    const [select_, setSelect_] = useState<string>('')
+
+
+
+    useEffect(() => {
+        if (select_ == 'populars') {
+            console.log('hola1')
+            // const newProducts: IProduct[] = _productsFiltered.filter(e => e.slug.includes('river') || e.slug.includes('boca'))
+            // setProductsFiltered(newProducts)
+        } else if (select_ == 'hight') {
+            console.log('hola2')
+            const newProducts: IProduct[] = _productsFiltered.sort((a: IProduct, b: IProduct) => {
+                if (a.price < b.price) {
+                    return 1
+                } else if (a.price, b.price) {
+                    return -1
+                }
+                return 0
+            })
+            setProductsFiltered(newProducts)
+        } else if (select_ == 'low') {
+            console.log('asdsadsa')
+            const newProducts: IProduct[] = _productsFiltered.sort((a: IProduct, b: IProduct) => {
+                if (a.price < b.price) {
+                    return -1
+                } else if (a.price, b.price) {
+                    return 1
+                }
+                return 0
+            })
+            setProductsFiltered(newProducts)
+        }
+    }, [select_])
+
 
 
     const handleClickValues = (valueOfProduct_: string) => {
@@ -29,6 +63,7 @@ export const ProductFilterPage = () => {
                 setValueProduct(valueOfProduct_)
                 setCategories(todasCategorias)
                 setTypeProduct('')
+                setSelect_('hight')
                 console.log(categories)
 
             } else if (valueOfProduct_ === 'regionales') {
@@ -123,6 +158,22 @@ export const ProductFilterPage = () => {
                     </Box>
                     <Divider sx={{ my: 1 }} />
                     <Box flex={1} />
+                </Box>
+                <Box>
+                    <FormControl sx={{ m: 3, minWidth: 120 }}>
+                        <InputLabel id="select-label">Sort By</InputLabel>
+                        <Select
+                            labelId="select-label"
+                            id="select"
+                            label="sort by"
+                            value=''
+                            onChange={e => setSelect_(e.target.value)}
+                        >
+                            <MenuItem value={'populars'}>Populars</MenuItem>
+                            <MenuItem value={'hight'}>Price: hight to low  </MenuItem>
+                            <MenuItem value={'low'}>Price: low to hight  </MenuItem>
+                        </Select>
+                    </FormControl>
                 </Box>
             </Box >
             {
