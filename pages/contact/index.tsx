30b47e8b97d7bb9ee07a11a40423e7901501a445
Jorge Ochipinti <jorgeochipinti@Air-de-Jorge.fibertel.com.ar>
2022-05-08 17:36:05 -0300

    sx

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -2,7 +2,7 @@ import { ShopLayout } from "../../components/layouts"
 import FormQuery from "../../components/ui/FormQuery"
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import MailIcon from '@mui/icons-material/Mail';
-import { Box, Link } from "@mui/material";
+import { Box, Divider, Link } from "@mui/material";
 import NextLink from 'next/link';
 
 const index = () => {
@@ -11,6 +11,7 @@ const index = () => {
       <FormQuery product_="" />
       <Box display='flex' justifyContent='center' >
         <Box display='flex' justifyContent='space-around' sx={{ mt: 5, width: 500 }}>
+          <Divider sx={{my:1}}/>
           <NextLink href='/' passHref>
             <Link>
               <WhatsAppIcon sx={{ fontSize: 50 }} />
