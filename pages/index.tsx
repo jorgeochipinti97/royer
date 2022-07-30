755ffc146f3ba7a6c3226f3e0c51d749b6956ac4
jorgeochipinti97 <jorgeochipinti97@gmail.com>
2022-07-30 13:57:07 -0300

    s

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,22 +4,61 @@ import { GetStaticProps } from 'next';
 
 import { ShopLayout } from '../components/layouts';
 
-import {  ProductList, ProductSlideshow } from '../components/products';
+import { ProductList, ProductSlideshow } from '../components/products';
 import { useProducts } from '../hooks';
 
 import { FullScreenLoading } from '../components/ui';
 import { useEffect, useState } from 'react';
 import { IProduct } from '../interfaces';
 import { dbProducts } from '../database';
+import { sortPopularity } from '../utils';
 
 
 
 interface Props {
-  product__: []
+  product__: IProduct[]
 }
 
 
 const HomePage: NextPage<Props> = ({ product__ }) => {
+
+  useEffect(() => {
+
+    const tshirts: IProduct[] = sortPopularity(product__, 'shirts')
+    const alfajores: IProduct[] = sortPopularity(product__, 'alfajores')
+    const wine: IProduct[] = sortPopularity(product__, 'wine')
+    const mate: IProduct[] = sortPopularity(product__, 'mate')
+    const accessories: IProduct[] = sortPopularity(product__, 'accessories')
+  
+    tshirts.sort((a: IProduct, b: IProduct) => {
+  
+      if (a.slug.indexOf('argentina_official_') < b.slug.indexOf('argentina_official_')) {
+        return 1
+      } else if (a.slug.indexOf('argentina_official_') > b.slug.indexOf('argentina_official_')) {
+        return -1
+      }
+      return 0
+    })
+  
+    const tShirtsMessi = tshirts.sort((a: IProduct, b: IProduct) => {
+  
+      if (a.slug.indexOf('messi') < b.slug.indexOf('messi')) {
+        return 1
+      } else if (a.slug.indexOf('messi') > b.slug.indexOf('messi')) {
+        return -1
+      }
+      return 0
+    })
+  
+  
+    const productos = tShirtsMessi
+      .concat(alfajores)
+      .concat(wine)
+      .concat(mate)
+      .concat(accessories)
+
+  setProducts_(productos)
+  })
 
   const { products, isLoading } = useProducts('/products');
   const [products_, setProducts_] = useState(product__)
@@ -42,7 +81,7 @@ const HomePage: NextPage<Props> = ({ product__ }) => {
                 <Typography variant="h1" sx={{ mb: 2, mt: 5 }}>Most Populars</Typography>
               </Box>
               <ProductList products={products_} />
-           
+
             </>
           )
       }
@@ -53,7 +92,7 @@ const HomePage: NextPage<Props> = ({ product__ }) => {
         <Typography variant='h1'>About Us</Typography>
       </Box>
       <Box sx={{ mt: 1 }} display='flex' justifyContent='center'>
-        <Typography variant='body1' align="justify" sx={{ width: 500,margin:3 }}>
+        <Typography variant='body1' align="justify" sx={{ width: 500, margin: 3 }}>
           Royer emerged at the end of 2021 as an idea, which we were able to carry out thanks to a lot of effort and dedication.
           Royers main objective is to make products known worldwide that could only be obtained locally, working together with Fedex and DHL this can be done safely and reliably.
           Royer is founded by only three members who each contributed their efforts to carry this out.</Typography>
