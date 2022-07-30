ae2dcba713da4f092ab45183924e0cb0a6213b1e
jorgeochipinti97 <jorgeochipinti97@gmail.com>
2022-07-30 14:04:58 -0300

    fix

--- components/ui/SideMenu.tsx
+++ components/ui/SideMenu.tsx
@@ -63,7 +63,7 @@ export const SideMenu = () => {
                         />
                     </ListItem>
               
-                    {
+                    {/* {
                         isLoggedIn && (
                             <>
                                 <ListItem button>
@@ -76,12 +76,12 @@ export const SideMenu = () => {
                                 </ListItem>
                             </>
                         )
-                    } 
+                    }  */}
 
 
 
 
-                    {
+                    {/* {
                         isLoggedIn
                             ? (
                                 <ListItem button onClick={logout}>
@@ -102,7 +102,7 @@ export const SideMenu = () => {
                                     <ListItemText primary={'Login'} />
                                 </ListItem>
                             )
-                    } 
+                    }  */}
                     <ListItem
                         button
                         sx={{ display: { xs: '', sm: 'none' } }}
