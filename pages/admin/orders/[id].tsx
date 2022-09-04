9d06028156ab7b7ddbe5b8f06a7d49f545ee3594
Jorge <jorge@Air-de-Jorge.fibertel.com.ar>
2022-09-04 01:47:00 -0300

    fix

--- pages/admin/orders/[id].tsx
+++ pages/admin/orders/[id].tsx
@@ -19,109 +19,111 @@ const OrderPage: NextPage<Props> = ({ order }) => {
     const { shippingAddress } = order;
 
 
-  return (
-    <AdminLayout 
-        title='Resumen de la orden' 
-        subTitle={ `OrdenId: ${ order._id }`}
-        icon={ <AirplaneTicketOutlined /> }
-    >
+    return (
+        <AdminLayout
+            title='Resumen de la orden'
+            subTitle={`OrdenId: ${order._id}`}
+            icon={<AirplaneTicketOutlined />}
+        >
 
-        {
-            order.isPaid
-            ? (
-                <Chip 
-                    sx={{ my: 2 }}
-                    label="Orden ya fue pagada"
-                    variant='outlined'
-                    color="success"
-                    icon={ <CreditScoreOutlined /> }
-                />
-            ):
-            (
-                <Chip 
-                    sx={{ my: 2 }}
-                    label="Pendiente de pago"
-                    variant='outlined'
-                    color="error"
-                    icon={ <CreditCardOffOutlined /> }
-                />
-            )
-        }
-
-        
-
-        <Grid container className='fadeIn'>
-            <Grid item xs={ 12 } sm={ 7 }>
-                <CartList products={  order.orderItems } />
-            </Grid>
-            <Grid item xs={ 12 } sm={ 5 }>
-                <Card className='summary-card'>
-                    <CardContent>
-                        <Typography variant='h2'>Resumen ({ order.numberOfItems } { order.numberOfItems > 1 ? 'productos': 'producto'})</Typography>
-                        <Divider sx={{ my:1 }} />
-
-                        <Box display='flex' justifyContent='space-between'>
-                            <Typography variant='subtitle1'>Dirección de entrega</Typography>
-                        </Box>
-
-                        
-                        <Typography>{ shippingAddress.firstName } { shippingAddress.lastName }</Typography>
-                        <Typography>{ shippingAddress.address } { shippingAddress.address2 ? `, ${ shippingAddress.address2 }`: '' }</Typography>
-                        <Typography>{ shippingAddress.city }, { shippingAddress.zip }</Typography>
-                        <Typography>{ shippingAddress.country }</Typography>
-                        <Typography>{ shippingAddress.phone }</Typography>
-                        <Typography>{ shippingAddress.email }</Typography>
-                        <Typography>{ shippingAddress.taxId }</Typography>
-
-                        <Divider sx={{ my:1 }} />
-
-
-                        <OrderSummary 
-                            orderValues={{
-                                numberOfItems: order.numberOfItems,
-                                total: order.total,
-                           
-                            }} 
+            {
+                order.isPaid
+                    ? (
+                        <Chip
+                            sx={{ my: 2 }}
+                            label="Orden ya fue pagada"
+                            variant='outlined'
+                            color="success"
+                            icon={<CreditScoreOutlined />}
                         />
+                    ) :
+                    (
+                        <Chip
+                            sx={{ my: 2 }}
+                            label="Pendiente de pago"
+                            variant='outlined'
+                            color="error"
+                            icon={<CreditCardOffOutlined />}
+                        />
+                    )
+            }
 
-                        <Box sx={{ mt: 3 }} display="flex" flexDirection='column'>
-                            {/* TODO */}
 
 
-                            <Box display='flex' flexDirection='column'>
-                                {
-                                    order.isPaid
-                                    ? (
-                                        <Chip 
-                                            sx={{ my: 2, flex: 1 }}
-                                            label="Orden ya fue pagada"
-                                            variant='outlined'
-                                            color="success"
-                                            icon={ <CreditScoreOutlined /> }
-                                        />
+            <Grid container className='fadeIn'>
+                <Grid item xs={12} sm={7}>
+                    <CartList products={order.orderItems} />
+                </Grid>
+                <Grid item xs={12} sm={5}>
+                    <Card className='summary-card'>
+                        <CardContent>
+                            <Typography variant='h2'>Resumen ({order.numberOfItems} {order.numberOfItems > 1 ? 'productos' : 'producto'})</Typography>
+                            <Divider sx={{ my: 1 }} />
 
-                                    ):(
-                                        <Chip 
-                                            sx={{ my: 2, flex: 1 }}
-                                            label="Pendiente de pago"
-                                            variant='outlined'
-                                            color="error"
-                                            icon={ <CreditCardOffOutlined /> }
-                                        />
-                                    )
-                                }
+                            <Box display='flex' justifyContent='space-between'>
+                                <Typography variant='subtitle1'>Dirección de entrega</Typography>
                             </Box>
 
-                        </Box>
 
-                    </CardContent>
-                </Card>
+                            <Typography>{shippingAddress.firstName} {shippingAddress.lastName}</Typography>
+                            <Typography>{shippingAddress.address} {shippingAddress.address2 ? `, ${shippingAddress.address2}` : ''}</Typography>
+                            <Typography>{shippingAddress.city}, {shippingAddress.zip}</Typography>
+                            <Typography>{shippingAddress.country}</Typography>
+                            <Typography>{shippingAddress.phone}</Typography>
+                            <Typography>{shippingAddress.email}</Typography>
+                            <Typography>{shippingAddress.taxId}</Typography>
+
+                            <Divider sx={{ my: 1 }} />
+
+
+                            <OrderSummary
+                                orderValues={{
+                                    numberOfItems: order.numberOfItems,
+                                    total: order.total,
+                                    precioFinal: order.total,
+                                    codigoDeDescuento: order.discountCode
+
+                                }}
+                            />
+
+                            <Box sx={{ mt: 3 }} display="flex" flexDirection='column'>
+                                {/* TODO */}
+
+
+                                <Box display='flex' flexDirection='column'>
+                                    {
+                                        order.isPaid
+                                            ? (
+                                                <Chip
+                                                    sx={{ my: 2, flex: 1 }}
+                                                    label="Orden ya fue pagada"
+                                                    variant='outlined'
+                                                    color="success"
+                                                    icon={<CreditScoreOutlined />}
+                                                />
+
+                                            ) : (
+                                                <Chip
+                                                    sx={{ my: 2, flex: 1 }}
+                                                    label="Pendiente de pago"
+                                                    variant='outlined'
+                                                    color="error"
+                                                    icon={<CreditCardOffOutlined />}
+                                                />
+                                            )
+                                    }
+                                </Box>
+
+                            </Box>
+
+                        </CardContent>
+                    </Card>
+                </Grid>
             </Grid>
-        </Grid>
 
 
-    </AdminLayout>
-  )
+        </AdminLayout>
+    )
 }
 
 
@@ -130,11 +132,11 @@ const OrderPage: NextPage<Props> = ({ order }) => {
 
 
 export const getServerSideProps: GetServerSideProps = async ({ req, query }) => {
-    
-    const { id = '' } = query;
-    const order = await dbOrders.getOrderById( id.toString() );
 
-    if ( !order ) {
+    const { id = '' } = query;
+    const order = await dbOrders.getOrderById(id.toString());
+
+    if (!order) {
         return {
             redirect: {
                 destination: '/admin/orders',
