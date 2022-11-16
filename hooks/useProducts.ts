f2ec64985e04e720a1d69975dbcda7399f287af1
Jorge <jorge@Air-de-Jorge.fibertel.com.ar>
2022-11-16 12:59:46 -0300

    s

--- hooks/useProducts.ts
+++ hooks/useProducts.ts
@@ -45,7 +45,7 @@ export const useProducts = (url: string, config: SWRConfiguration = {}) => {
         const yerba: IProduct[] = sortPopularity(products, 'yerba')
         const espadrilles: IProduct[] = sortPopularity(products, 'espadrilles')
         const footwear: IProduct[] = sortPopularity(products, 'footwear')
-
+ 
         return tShirtsMessi
             .concat(shorts)
             .concat(hoodies)
